9375a9d7ab22dd353a339f279d7f19cf7ab8ee47
Ryan Febriansyah <15523163@students.uii.ac.id>
2019-04-10 22:53:37 +0700

    fixed Specifying a namespace in urls

--- conduit/urls.py
+++ conduit/urls.py
@@ -19,7 +19,7 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
 
-    url(r'^api/', include('conduit.apps.articles.urls', namespace='articles')),
-    url(r'^api/', include('conduit.apps.authentication.urls', namespace='authentication')),
-    url(r'^api/', include('conduit.apps.profiles.urls', namespace='profiles')),
+    url(r'^api/', include('conduit.apps.articles.urls'), name='articles'),
+    url(r'^api/', include('conduit.apps.authentication.urls'), name='authentication'),
+    url(r'^api/', include('conduit.apps.profiles.urls'), name='profiles'),
 ]
