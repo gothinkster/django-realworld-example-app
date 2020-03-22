e12137977f517aa2f5b5f5fe653e2eb62036c275
Javier Wilson <javier.wilson@gmail.com>
2020-03-22 02:07:07 -0600

    static root

--- conduit/settings.py
+++ conduit/settings.py
@@ -128,6 +128,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'static')
+
 
 CORS_ORIGIN_WHITELIST = (
     '0.0.0.0:4000',
