01bb94de2bf47389d3c4153c6fccb9f8ec9325c6
Javier Wilson <javier.wilson@gmail.com>
2020-03-22 01:43:18 -0600

    allow all

--- conduit/settings.py
+++ conduit/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '2^f+3@v7$v1f8yt0!s)3-1t$)tlp+xm17=*g))_xoi&&9m#2a&'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
